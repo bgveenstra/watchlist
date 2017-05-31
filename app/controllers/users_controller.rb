f9015fe62bc60a856491db68f9a99cc0b2e6cc72
sejinpark0209 <sejinpark0209@gmail.com>
2017-05-31 11:23:36 -0700

    dry code for profile function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
-  before_action :check_user
 
-  private
-  def check_user
+  def show
     @user = User.find(params[:id])
     if current_user != @user
       redirect_to root_url
