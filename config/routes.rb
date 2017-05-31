45783020e345b4b3d2241cefd6f2184c2a150e48
sejinpark0209 <sejinpark0209@gmail.com>
2017-05-30 21:10:24 -0700

    added profile page but need fix showing profile for others

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
 
   resources :movies
   devise_for :users
+  resources :users
   root "index#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
