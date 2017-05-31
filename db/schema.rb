eda2e0fe1f3f94248e5c2f0c4f2db5f50f286512
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-05-31 15:37:03 -0700

    changed to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170531001857) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.string "type"
