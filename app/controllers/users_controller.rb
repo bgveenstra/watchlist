87c55c7a8d878275a8ba7fb4bba63aa823945199
sejinpark0209 <sejinpark0209@gmail.com>
2017-05-30 21:42:51 -0700

    fixed users controller. only shows correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  
-  def show
+  before_action :check_user
+
+  private
+  def check_user
     @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   resources :movies
   devise_for :users
-  resources :users
+  resources :users, only: [:show]
+  get 'users/show'
   root "index#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
