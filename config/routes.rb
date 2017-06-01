87106c29257b2a8879ee4583d2c7ae6ac4f10cbc
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-06-01 13:41:50 -0700

    Scaffolded list

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :lists
   # root "application#index"
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :movies
   resources :users, only: [:show]
-  get 'users/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
