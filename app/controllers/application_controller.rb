7557f226a563a880e2f4e500353483d95033e860
sejinpark0209 <sejinpark0209@gmail.com>
2017-06-01 20:36:19 -0700

    after sign up, sign out, redirects to correct page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def after_sign_in_path_for(resource)
+    current_user_path
+  end
 
-  def index	
+  def index
   	render "../views/layouts/application.html.erb"
   end
 
