b44f9332ee2bd6359190ea81d15ccb7bd47ea0d1
sejinpark0209 <sejinpark0209@gmail.com>
2017-06-02 15:16:29 -0700

    solved route and redirect issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def after_sign_in_path_for(resource)
-    current_user_path
-  end
 
   def index
   	render "../views/layouts/application.html.erb"
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    current_user
+  end
+
+  def after_sign_up_path_for(resource_or_scope)
+    current_user
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+   new_user_registration_path
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,23 +6,4 @@ class UsersController < ApplicationController
       redirect_to root_url
     end
   end
-  def create
- @user = User.new(user_params)
- if @user.save
-  log_in @user
-  redirect_to @user
-  flash[:success] = "Welcome!"
- else
-  render 'new'
-end
-
-  # self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-  # if resource.errors.empty?
-  #   set_flash_message(:notice, :confirmed) if is_flashing_format?
-  #   sign_in(resource_name, resource)
-  #   respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
-  # else
-  #   redirect_to new_user_session_path
-  #   #respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
-  # end
 end
