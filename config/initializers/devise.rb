651d7563b19d3f94e7b152e07e68811bf1c4f4e8
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-06-03 11:58:09 -0700

    Updated user model to handle oAuth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'mailer@herokuapp.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
@@ -249,12 +249,6 @@ Devise.setup do |config|
 
   # ==> OmniAuth
 
-  # Use integrating OAuth using Google accounts
-
-  config.omniauth :google_oauth2, ENV["225587889150-6csctslusmcud2d34keiffegeai5s246.apps.googleusercontent.com"], ENV["Cs6oAVvvM81afps9dDcWbH80"], {
-    scope: "email"
-  }
-
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
@@ -281,4 +275,11 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+
+  # Use integrating OAuth using Google accounts
+
+  config.omniauth :google_oauth2, ENV["225587889150-6csctslusmcud2d34keiffegeai5s246.apps.googleusercontent.com"], ENV["Cs6oAVvvM81afps9dDcWbH80"], {
+    scope: "email"
+  }
+  
 end
