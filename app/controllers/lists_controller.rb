d288cfaf35c9cd8291cbff9740f62a1283cdb08d
sejinpark0209 <sejinpark0209@gmail.com>
2017-06-05 13:10:53 -0700

    test

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,30 +1,31 @@
 class ListsController < ApplicationController
   before_action :set_list, only: [:show, :edit, :update, :destroy]
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  
+
   # GET /lists
   # GET /lists.json
   def index
-    user = set_user
-    @lists = user.lists.all
+    @user = User.find(params[:user_id])
+    @lists = @user.lists.all
   end
 
   # GET /lists/1
   # GET /lists/1.json
   def show
-    @user = set_user
-    @list = set_list
+    @user = User.find(params[:user_id])
+    @list = List.find(params[:id])
+    render :show
   end
 
   # GET /lists/new
   def new
     @list = List.new
+    @user = User.find(params[:user_id]).id
   end
 
   # GET /lists/1/edit
   def edit
-    @user = set_user
-    @list = set_list
+    @user = User.find(params[:user_id])
+    @list = List.find(params[:id])
   end
 
   # POST /lists
@@ -32,14 +33,15 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params_create)
 
-    if @list.save!
-      flash[:notice] = "List saved successfully!"
-      redirect_to user_lists_path
-    else
-      flash[:error] = @list.errors.full_messages.join(", ")
-      redirect_to user_lists_path(@user)
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to user_lists_url, notice: 'List was successfully created.' }
+        format.json { render :show, status: :created, location: @list }
+      else
+        format.html { render :new }
+        format.json { render json: @list.errors, status: :unprocessable_entity }
+      end
     end
-
   end
 
   # PATCH/PUT /lists/1
@@ -73,13 +75,11 @@ class ListsController < ApplicationController
     end
     # list param for create without require list
     def list_params_create
-      params.permit(:name, :user_id, lists_attributes: [:name, :user_id])
+      params.permit(:name, :user_id, lists_attributes: [:id])
     end
     # list params for update with require list
     def list_params_update
-      params.require(:list).permit(:name, :user_id, lists_attributes: [:id,  :_destroy])
+      params.require(:list).permit(:name, :user_id, lists_attributes: [:id])
     end
-    def set_user
-      @user = User.find(params[:user_id])
-    end
+
 end
